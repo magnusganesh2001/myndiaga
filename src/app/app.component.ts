51d970b0e368d45250854e33fca010a468e75b3d
Sarthak garg <60544034+sarthakgarg1009@users.noreply.github.com>
2021-10-19 12:30:48 +0530

    side bar changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Inbox', icon: 'home' },
+    { title: 'All Category', url: '/folder/Outbox', icon: 'grid' },
+    { title: 'My Orders', url: '/folder/Favorites', icon: 'push' },
+    { title: 'My Cart', url: '/folder/Archived', icon: 'cart' },
+    { title: 'My Wishlist', url: '/folder/Trash', icon: 'heart' },
+    { title: 'My Account', url: '/folder/Spam', icon: 'person' },
+
+    { title: 'Pan Card', url: '/folder/pan', icon: 'card' },
+    { title: 'Change Password', url: '/folder/password', icon: 'key' },
+
+    { title: 'Address', url: '/folder/address', icon: 'locate' },
+    { title: 'Bank Details', url: '/folder/cash', icon: 'cash' },
+    { title: 'Rating And Reviews', url: '/folder/rating', icon: 'star' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
